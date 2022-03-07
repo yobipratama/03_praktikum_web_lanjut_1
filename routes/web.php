66a60ea004115d40aa1299505e414404b8af803e
yobipratama <yobir231@gmail.com>
2022-03-07 21:41:20 +0700

    Jobsheet3

---
+++ routes/web.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('index');
+})->name('home');;
+
+Route::get('about', function () {
+    return view('about');
+})->name('about');;
+
+Route::get('rooms', function () {
+    return view('rooms');
+})->name('rooms');;
+
+Route::get('gallery', function () {
+    return view('gallery');
+})->name('gallery');;
+
+Route::get('dinning', function () {
+    return view('dinning');
+})->name('dinning');;
+
+Route::get('news', function () {
+    return view('news');
+})->name('news');;
+
+Route::get('contact', function () {
+    return view('contact');
+})->name('contact');;
